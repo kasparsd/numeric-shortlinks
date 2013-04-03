90ef5e340c26f38cabd86a2b62b59d05704b61ac
Kaspars <kaspars@konstruktors.com>
2013-04-03 13:46:33 +0200

    Simplify things

--- numeric-shortlinks.php
+++ numeric-shortlinks.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Numeric Shortlinks
 Description: Adds support for numeric shortlinks like <code>http://example.com/123</code>
-Version: 2.5
+Version: 1.1
 Author: Kaspars Dambis	
 */
 
@@ -10,32 +10,24 @@ Author: Kaspars Dambis
 add_filter( 'pre_get_shortlink', 'numeric_shortlink_head', 10, 4 );
 
 function numeric_shortlink_head( $return, $id, $context, $slugs ) {
-	global $wp_query, $post;
-	
-	if ( $wp_query->is_singular() && ! $wp_query->is_front_page() || is_admin() ) {
-		if ( is_admin() )
-			$post_id = $post->ID;
-		else
-			$post_id = $wp_query->get_queried_object_id();
+	if ( is_singular() ) 
+		return home_url( '/' . get_queried_object_id() );
 
-		return get_bloginfo('home') . '/' . $post_id;
-	}
-	
 	return $return;
 }
 
 
-add_action( 'pre_get_posts', 'numeric_shortlink_redirect' );
+add_action( 'template_redirect', 'maybe_numeric_shortlink_redirect' );
 
-function numeric_shortlink_redirect( $query ) {
-	if ( ! $query->is_main_query() )
-		return;
+function maybe_numeric_shortlink_redirect() {
+	// Get the trailing part of the URI
+	$maybe_post_id = end( explode( '/', $_SERVER['REQUEST_URI'] ) );
 
-	$uri_int = trim( $_SERVER['REQUEST_URI'], '/' );
-
-	if ( ! is_numeric( $uri_int ) )
+	// Check if it is numeric
+	if ( ! is_numeric( $maybe_post_id ) )
 		return;
 	
+	// Redirect
 	if ( $post_url = get_permalink( $uri_int ) ) {
 		wp_redirect( $post_url, 301 );
 		exit;	
