c5f5f8a437316b236cd97beedb38cf109b0e980c
Kaspars <kaspars@konstruktors.com>
2013-04-06 23:39:45 +0200

    Add a few filters

--- README.md
+++ README.md
@@ -4,6 +4,33 @@
 
 This plugin doesn't have any configuration options. It automatically hooks into the `wp_get_shortlink()` function and adds the new URL both in HTML `<head>` and any plugin that relies on shortlink functionality.
 
+## Available Filters
+
+### numeric_shortlinks_to_url
+
+Create your own mapping to post ID. Used when creating a shortlink.
+
+	add_filter( 'numeric_shortlinks_to_url', 'my_shortlink_to_url' );
+
+	function my_shortlink_to_url( $post_id ) {
+		// Do something to $post_id
+		// The resulting URL will be http://example.com/$post_id
+		return $post_id;
+	}
+
+### numeric_shortlinks_to_id
+
+Map the trailing part of URL to post ID. Used when resolving a shortlink.
+
+	add_filter( 'numeric_shortlinks_to_id', 'my_shortlink_to_id' );
+
+	function my_shortlink_to_id( $slug ) {
+		// Convert $slug to a numeric $post_id
+		return $slug;
+	}
+
+## Author
+
 Created by **Kaspars Dambis**:  
-[konstruktors.com](http://konstruktors.com)
+[konstruktors.com](http://konstruktors.com)  
 [@konstruktors](http://twitter.com/konstruktors)
--- numeric-shortlinks.php
+++ numeric-shortlinks.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Numeric Shortlinks
 Description: Adds support for numeric shortlinks like <code>http://example.com/123</code>
-Version: 1.3
+Version: 1.4
 Author: Kaspars Dambis	
 */
 
@@ -11,8 +11,11 @@ add_filter( 'pre_get_shortlink', 'numeric_shortlink_head', 10, 4 );
 
 function numeric_shortlink_head( $return, $id, $context, $slugs ) {
 	if ( is_singular() ) 
-		return home_url( '/' . get_queried_object_id() );
-	elseif ( 'post' == $context )
+		$id = get_queried_object_id();
+
+	$id = apply_filters( 'numeric_shortlinks_to_url', $id );
+	
+	if ( is_numeric( $id ) )
 		return home_url( '/' . $id );
 
 	return $return;
@@ -23,7 +26,7 @@ add_action( 'template_redirect', 'maybe_numeric_shortlink_redirect' );
 
 function maybe_numeric_shortlink_redirect() {
 	// Get the trailing part of the URI
-	$maybe_post_id = end( explode( '/', trim( $_SERVER['REQUEST_URI'], '/' ) ) );
+	$maybe_post_id = apply_filters( 'numeric_shortlinks_to_id', end( explode( '/', trim( $_SERVER['REQUEST_URI'], '/' ) ) ) );
 
 	// Check if it is numeric
 	if ( ! is_numeric( $maybe_post_id ) )
